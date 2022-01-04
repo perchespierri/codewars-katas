8d9572a71b5b50b0bb3a1a611014c828af793650
perchespierri <perchespierri@gmail.com>
2022-01-04 11:05:13 -0300

    My two Katas for now

---
+++ Kata 1 - jan 2 2022 copy.js
@@ -0,0 +1,33 @@
+function toCamelCase(str){
+  
+  const noDash = str.split('-');
+  const noUnderscoreOrDashes = [];
+  noDash.forEach(word => {
+    const newArrayElement = word.split('_');
+    noUnderscoreOrDashes.push(...newArrayElement);
+  });
+
+  const finalStr = noUnderscoreOrDashes.map((word, index) => {
+    if (index >= 1) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    } else return word;
+  });
+
+  return finalStr.join('');
+}
+
+console.log(
+  toCamelCase('hello_wait-bye')
+);
+
+
+// I solved it without regex.
+// An example using it:
+
+function toCamelCase(str){
+  return str.replace(/[^a-z]./gi, word => word.toUpperCase().slice(-1))
+}
+
+console.log(
+  toCamelCase('hello_wait-bye')
+);
